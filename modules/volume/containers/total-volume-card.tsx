3d43bbf23119c86b441ea1215c3ab6fe0c616d92
Vadim Tarasenko <vadim@wormsconnect.com>
2025-11-13 23:50:36 +0200

    feat: update home screen

--- modules/volume/containers/total-volume-card.tsx
+++ modules/volume/containers/total-volume-card.tsx
@@ -6,7 +6,7 @@ export function TotalVolumeCard() {
   const { data: totalVolume = 0 } = useTotalVolumeQuery();
   return (
     <View className="mx-4 bg-blue-500 rounded-3xl shadow-md relative">
-      <View className="relative p-6 h-40 flex-1 rounded-3xl overflow-hidden">
+      <View className="relative p-6 flex-1 rounded-3xl overflow-hidden">
         <View className="absolute -left-[100px] -top-[240px] w-[350px] h-[350px] bg-white/10 rounded-full" />
 
         <View className="flex-row justify-between items-center">
